198b5de17daec9f22b44af7d08be901c5abfbccc
Xuffasch <tristan.lucas@gmail.com>
2021-08-28 16:10:00 +0200

    Add html on the initial page

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,1 +1,126 @@
 <h1>Svelte To-do list</h1>
+
+<!-- Form  -->
+<div class="todoapp  stack-large">
+  <form>
+    <h2 class="label-wrapper">
+      <label for="todo-0" class="label__lg">What needs to be done ?</label>
+    </h2>
+    <input type="text" id="todo-0" autocomplete="off" class="input input__lg" />
+    <button type="submit" disabled="" class="btn btn __primary btn__lg">
+      Add
+    </button>
+  </form>
+
+  <!-- Filter on items to display -->
+  <div class="filters btn-group stack-exception">
+    <button class="btn toggle-btn" aria-pressed="true">
+      <span class="visually-hidden">Show</span>
+      <span>All</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+    <button class="btn toggle-btn" aria-pressed="false">
+      <span class="visually-hidden">Show</span>
+      <span>Active</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+    <button class="btn toggle-btn" aria-pressed="false">
+      <span class="visually-hidden">Show</span>
+      <span>Completed</span>
+      <span class="visually-hidden">tasks</span>
+    </button>
+  </div>
+
+  <!-- Infos on displayed items -->
+  <h2 id="list-heading">2 out of 3 itemps completed</h2>
+
+  <!-- Todos -->
+  <ul role="list" class="" aria-labelledby="list-heading">
+    <li class="todo">
+      <div class="stack-small">
+        <form class="stack-small">
+          <div class="form-group">
+            <label for="todo-1" class="todo-label">
+              New name for 'Create a svelte starter'
+            </label>
+            <input
+              type="text"
+              id="todo-1"
+              autocomplete="off"
+              class="todo-text"
+            />
+            <div class="btn-group">
+              <button class="btn todo-cancel" type="button">
+                Cancel
+                <span class="visually-hidden">
+                  renaming Create a Svelte starter app
+                </span>
+              </button>
+              <button class="btn btn__primary todo-edit" type="submit">
+                Save
+                <span class="visually-hidden">
+                  new name for Create a Svelte starter app
+                </span>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </li>
+
+    <li class="todo">
+      <div class="stack-small">
+        <form class="stack-small">
+          <div class="form-group">
+            <input type="checkbox" id="todo-2" checked />
+            <label for="todo-2" class="todo-label">
+              Create your first component
+            </label>
+            <div class="btn-group">
+              <button type="button" class="btn">
+                Edit
+                <span class="visually-hidden">Create your first component</span>
+              </button>
+              <button type="button" class="btn btn__danger">
+                Delete
+                <span class="visually-hidden">
+                  Create your first component
+                </span>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </li>
+
+    <li class="todo">
+      <div class="stack-small">
+        <form class="stack-small">
+          <div class="form-group">
+            <input type="checkbox" id="todo-3" />
+            <label for="todo-3" class="todo-label">
+              Complete the rest of the tutorial
+            </label>
+            <div class="btn-group">
+              <button type="button" class="btn">
+                Edit
+                <span class="visually-hidden">Create your first component</span>
+              </button>
+              <button type="button" class="btn btn__danger">
+                Delete
+                <span class="visually-hidden">
+                  Create your first component
+                </span>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </li>
+  </ul>
+
+  <div class="btn-group">
+    <button type="button" class="btn btn__primary">Check all</button>
+    <button type="button" class="btn btn__primary">Remove completed</button>
+  </div>
+</div>
